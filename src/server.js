71f05c53a3932d2f949a7ae05f769deb27556a95
seungho Jeon <urijsh612@gmail.com>
2020-01-14 16:22:36 +0900

    #1.1 Graphql 서버 생성

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import { GraphQLServer } from "graphql-yoga";
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "안녕"
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: PORT }, () =>
+  console.log(`GraphQL서버 시작 ✅ http://localhost:${PORT}`)
+);
