7490b3c16a46d0556bd2f80d32c26a703bc33b68
seungho Jeon <urijsh612@gmail.com>
2020-01-30 10:54:43 +0900

    주석수정

--- src/api/Notice/seeNotice/seeNotice.js
+++ src/api/Notice/seeNotice/seeNotice.js
@@ -7,7 +7,6 @@ export default {
       const { name } = args;
       try {
         const notice = await prisma.notices({ name });
-        console.log("출력값", notice);
         return notice;
       } catch (error) {
         console.log("공지열람 오류::", error);
