a633e1b72c4185bf48d2a600211dabc899fa5511
seungho Jeon <urijsh612@gmail.com>
2020-02-05 22:52:01 +0900

    공지 에러 검사

--- src/api/Notice/seeNotice/seeNotice.js
+++ src/api/Notice/seeNotice/seeNotice.js
@@ -3,13 +3,14 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeNotice: async (_, args, { request, isAuthenticated }) => {
-      // isAuthenticated(request);
+      isAuthenticated(request);
       const { name } = args;
       try {
         const notice = await prisma.notices({ name });
         return notice;
       } catch (error) {
-        console.log("공지열람 오류::", error);
+        // console.log("공지열람 오류::", error);
+        // null;
       }
     }
   }
